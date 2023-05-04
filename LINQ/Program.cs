a29cd29ac264de1ed587f0cca2ead3782116762e
xmilak <126503532+xmilak@users.noreply.github.com>
2023-05-03 21:03:03 -0400

    Done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace LINQ
+﻿using System.Globalization;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>()
+           { "Zelda", "Fortnite", "Borderlands", "Metal Gear Solid", "Ratchet and Clank", "Zak", "Fallout", "Marathon", "Minecraft" };
+
+            
+            //Organize by length of name
+            
+            var videoGameOrder = videoGames.OrderBy(name => name.Length);
+
+            foreach( var videoGameList in videoGameOrder) 
+            {
+                Console.WriteLine(videoGameList);
+            }
+
+            //OR in a single line, like so. 
+             videoGameOrder.OrderBy(name =>name.Length).ToList().ForEach(Console.WriteLine); 
+             
+
+
         }
+
     }
 }
